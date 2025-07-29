fc4cb032598a55c3ab5311da9f1b23e70a5158aa
Kirsty Tidmarsh <tidmarshk@macbookair.home>
2025-07-29 23:28:10 +0100

    Initial commit: HolidaySearch project with NUnit tests

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HolidaySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Holiday Search!");
+        }
+    }
+}
---
+++ UnitTests/HolidaySearchTests.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+
+namespace HolidaySearch
+{
+    [TestFixture]
+    public class HolidaySearchTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // This is a placeholder test. Replace with actual logic as needed.
+            Assert.That(true, Is.True);
+        }
+    }
+}
