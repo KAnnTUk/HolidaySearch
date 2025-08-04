bf85ef0ca308bd2f92b2aa5d8a0dc5b8d3cf484a
“KAnnTUK” <kirst2k@hotmail.com>
2025-08-04 16:32:22 +0100

    Implemented hotel search by arrival date and duration

--- ConsoleApp/Hotels.cs
+++ ConsoleApp/Hotels.cs
@@ -28,5 +28,16 @@ namespace ConsoleApp
             var hotels = JsonSerializer.Deserialize<List<Hotel>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             return hotels ?? new List<Hotel>();
         }
+
+        // Searches for hotels based on arrival date and number of nights
+        public static List<Hotel> SearchForHotels(string arrivalDate, int nights)
+        {
+            return ReadHotelsFromJson()
+            .Where(h =>
+                h.arrival_date.Equals(arrivalDate, StringComparison.OrdinalIgnoreCase) &&
+                h.nights == nights
+            )
+            .ToList();
+        }
     }
 }
