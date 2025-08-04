48766db190f3cc8e89aaff2678ed5c5f5fb5c3a0
“KAnnTUK” <kirst2k@hotmail.com>
2025-08-04 16:17:48 +0100

    Retrieval of all hotels form the json file

---
+++ ConsoleApp/Hotels.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleApp
+{
+    public class Hotel
+    {
+        private static readonly string filePath = "Data/Hotel.json";
+
+        #region Properties
+        public int id { get; set; }
+        public string name { get; set; }
+        public string arrival_date { get; set; }
+        public decimal price_per_night { get; set; }
+        public string[] local_airports { get; set; }
+        public int nights { get; set; }
+        #endregion
+
+        // Reads a list of hotels from a JSON file
+        public static List<Hotel> ReadHotelsFromJson()
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File not found: {filePath}");
+
+            var json = File.ReadAllText(filePath);
+            var hotels = JsonSerializer.Deserialize<List<Hotel>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            return hotels ?? new List<Hotel>();
+        }
+    }
+}
